a157042f74fb9758a2d86911ca39d869a9cee1a9
zhicao123 <254707268@qq.com>
2019-12-17 16:45:37 +0800

    命名空间

---
+++ 1.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\User;
+
+include "User.php"
+
+echo User::make();
+
+?>
---
+++ User.php
@@ -0,0 +1,11 @@
+<?php
+namespace App;
+
+class User{
+
+    public static function make()
+    {
+        return __METHOD__;
+    }
+}
+?>
